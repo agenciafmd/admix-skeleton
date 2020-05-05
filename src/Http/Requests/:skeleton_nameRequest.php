3d9bb03cdebd8588068b8c268a569aa4f8e607e1
Irineu Junior <irineu@fmd.ag>
2020-05-05 15:41:55 -0300

    feat(request): corrige errorBag

--- src/Http/Requests/:skeleton_nameRequest.php
+++ src/Http/Requests/:skeleton_nameRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class :skeleton_nameRequest extends FormRequest
 {
+    protected $errorBag = 'admix';
+
     public function rules()
     {
         return [
