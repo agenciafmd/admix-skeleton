d92aae478e185cb74538180ff863f0fe1357ced0
Irineu Junior <irineu@mixd.com.br>
2021-02-05 18:03:50 -0300

    feat(cache) acrescenta a limpeza de cache no observer

--- src/Observers/:skeleton_nameObserver.php
+++ src/Observers/:skeleton_nameObserver.php
@@ -4,6 +4,8 @@ namespace :namespace_vendor\:namespace_skeleton_name\Observers;
 
 use :namespace_vendor\:namespace_skeleton_name\Models\:skeleton_name;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 
 class :skeleton_nameObserver
 {
@@ -14,6 +16,38 @@ class :skeleton_nameObserver
 
     public function saved(:skeleton_name $model)
     {
-        //
+        if (!app()->runningInConsole()) {
+
+            /* descomente caso for utilizar, não faça cache da listagem (ex. frontend.articles.index)
+            try {
+                dispatch(function () use ($model) {
+                    Artisan::call('page-cache:clear', [
+                        'slug' => 'pc__index__pc',
+                    ]);
+
+                    Http::get(url('/'));
+                })
+                    ->delay(now()->addSeconds(5))
+                    ->onQueue('low');
+            } catch (\Exception $exception) {
+                // não tem problema
+            }
+
+            try {
+                dispatch(function () use ($model) {
+                    $url = str_replace(config('app.url') . '/', '', $model->url);
+                    Artisan::call('page-cache:clear', [
+                        'slug' => $url,
+                    ]);
+
+                    Http::get($model->url);
+                })
+                    ->delay(now()->addSeconds(5))
+                    ->onQueue('low');
+            } catch (\Exception $exception) {
+                // não tem problema
+            }
+            */
+        }
     }
 }
